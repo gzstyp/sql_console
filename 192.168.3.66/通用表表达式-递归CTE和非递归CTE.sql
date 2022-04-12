6bf5ca7af39009529fcbf8e2914b4a4e912dca9b
引路者 <444141300@qq.com>
2022-04-12 12:09:45 +0800

    递归

--- 192.168.3.66/通用表表达式-递归CTE和非递归CTE.sql
+++ 192.168.3.66/通用表表达式-递归CTE和非递归CTE.sql
@@ -11,11 +11,21 @@ with cte1(id) as (select 1),cte2(kid) as (select id+1 from cte1) -- 直接引用
 -- 当然也可以定义cte3或一直定义下去；完成定义之后再使用他们,可以重复使用前面已定义好的内容字段
 select * from cte1 join cte2 -- 交叉连接,即笛卡尔集
 
--- 使用
+-- 使用,其中 cte 是定义了一个表的名称,v是表cte的字段名
 with recursive cte(v) as -- cte 是定义了一个表的名称,v是字段名
 	(
-		select 1
-		UNION all
-		select v+1 from cte where v < 5 -- v是字段名,提现了递归调用的概念
+		select 1 -- 递归的初始化
+		UNION all -- 开始我们的递归
+		select v+1 from cte where v < 5 -- 查询条件,v是字段名,提现了递归调用的概念
 	)
 select * from cte;-- cte 是表的名称
+
+select kid, name,pid from sys_menu;
+
+with recursive employee_paths(id,name,path) as (
+    select id,name cast(id as char(200))-- 初始化
+    from sys_menu where pid = '88888888888888888888888888888888'
+    union all
+
+)
+
